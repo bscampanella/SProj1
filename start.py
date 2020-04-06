8c97b206e98b2a6ec51d64d70b1eaaf480640b28
Brandon C <bscampnaella99@gmail.com>
2020-04-06 15:46:52 -0700

    knn finish for my purposes moving on

--- start.py
+++ start.py
@@ -1,3 +1,7 @@
-import data_utils
+import data_utils as du
+import matplotlib.pyplot as plt
 
-
+dataDict = du.get_CIFAR10_data()
+xTrain = dataDict["X_train"]
+print(xTrain.shape)
+print(xTrain.shape[0])
